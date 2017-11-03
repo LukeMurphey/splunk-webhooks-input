f36b77975011bc2baa21a010e24aa749a262ce30
LukeMurphey <Luke@LukeMurphey.net>
2017-11-03 00:18:21 -0500

    Adding tests for non-SSL connections

--- tests/unit.py
+++ tests/unit.py
@@ -14,7 +14,7 @@ from webhook import WebServer
 sys.path.append(os.path.join("lib"))
 import HtmlTestRunner
 
-class WebhooksAppTest(unittest.TestCase):
+class WebhooksAppTest(object):
     """
     This provides some functionality for testing the webhooks app, including:
 
@@ -29,16 +29,24 @@ class WebhooksAppTest(unittest.TestCase):
 
     httpd = None
     thread = None
-    path = '/test_run_webserver_with_ssl'
+    path = '/test_run_webserver'
     results = None
+    protocol = 'http'
 
     def toInt(self, str_int):
+        """
+        Convert the string to an integer but return None if the value is None.
+        """
+
         if str_int is None:
             return None
         else:
             return int(str_int)
 
     def loadConfig(self, properties_file=None):
+        """
+        Load the configuration from the local.properties file or the environment variables.
+        """
 
         # Stop if we already loaded the configuration
         if WebhooksAppTest.config_loaded:
@@ -75,7 +83,7 @@ class WebhooksAppTest(unittest.TestCase):
             WebhooksAppTest.port = settings.get("value.test.server_port", WebhooksAppTest.port)
 
     @classmethod
-    def startServerThread(cls):
+    def start_server(cls):
         """
         Create an instance of the webhooks web-server running SSL.
 
@@ -89,8 +97,14 @@ class WebhooksAppTest(unittest.TestCase):
         """
 
         path_re = cls.path + '/*'
-        cert_file = os.path.abspath('certs/server.crt')
-        key_file = os.path.abspath('certs/server.key')
+
+        if cls.protocol == 'https':
+            cert_file = os.path.abspath('certs/server.crt')
+            key_file = os.path.abspath('certs/server.key')
+        else:
+            cert_file = None
+            key_file = None
+
         logger = logging.getLogger('test_webhooks')
 
         def output_results(results):
@@ -102,50 +116,73 @@ class WebhooksAppTest(unittest.TestCase):
 
         cls.results = []
         cls.httpd = WebServer(output_results, cls.port, path_re, cert_file, key_file, logger=logger)
+        cls.httpd.start_serving()
 
     @classmethod
     def setUp(cls):
+        """
+        Set up the class by clearing the results for the next test.
+        """
+
+        # Clear the results so that the tests don't interact with each other
         if cls.results is not None:
             cls.results[:] = []
 
     @classmethod
-    def setUpClass(cls):
-
-        if cls.thread is None:
-            cls.thread = threading.Thread(target=cls.startServerThread)
-            cls.thread.setDaemon(True)
-            cls.thread.start()
-
-class TestSSL(WebhooksAppTest):
-    """
-    this tests the input ability to work with SSL.
-    """
-
-    def test_run_webserver_with_ssl_get(self):
+    def start_server_thread(cls):
         """
-        Try to run the webhooks input with SSL to ensure that it loads correctly using a GET
+        Start the server thread.
+        """
+
+        # Stop the existing server if necessary
+        if cls.httpd is not None:
+            cls.httpd.stop_serving()
+
+        # Start the new one
+        cls.thread = threading.Thread(target=cls.start_server)
+        cls.thread.setDaemon(True)
+        cls.thread.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Shut down the server gracefully.
+        """
+
+        if cls.httpd is not None:
+            cls.httpd.stop_serving()
+
+class TestWebhooksServer(WebhooksAppTest, unittest.TestCase):
+    """
+    This tests the input ability to accept data. By default, it will serve a non-SSL connection.
+    """
+    protocol = 'http'
+
+    def test_run_webserver_with_get(self):
+        """
+        Try to run the webhooks input and ensure that it loads correctly using a GET
         request.
         """
 
-        url = 'https://127.0.0.1:' + str(self.port) + self.path \
-        + '/TEST?test_run_webserver_with_ssl_get=SOMETESTVALUE'
+        url = self.protocol + '://127.0.0.1:' + str(self.port) + self.path \
+        + '/TEST?test_run_webserver_with_get=SOMETESTVALUE'
         response = requests.get(url, verify=False, timeout=5)
         response_parsed = json.loads(response.text)
 
         self.assertEquals(response_parsed['success'], True)
         self.assertEquals(response.status_code, 200)
 
-        self.assertEquals(self.results[0]['test_run_webserver_with_ssl_get'][0], 'SOMETESTVALUE')
+        self.assertEquals(self.results[0]['test_run_webserver_with_get'][0], 'SOMETESTVALUE')
 
-    def test_run_webserver_with_ssl_post(self):
+    def test_run_webserver_with_post(self):
         """
-        Try to run the webhooks input with SSL to ensure that it loads correctly using a POST
+        Try to run the webhooks input and ensure that it loads correctly using a POST
         request.
         """
 
-        url = 'https://127.0.0.1:' + str(self.port) + self.path + '/TEST'
+        url = self.protocol + '://127.0.0.1:' + str(self.port) + self.path + '/TEST'
         data = {
-            'test_run_webserver_with_ssl_post':'SOMETESTVALUE'
+            'test_run_webserver_with_post':'SOMETESTVALUE'
         }
 
         response = requests.post(url, verify=False, data=data, timeout=5)
@@ -153,7 +190,18 @@ class TestSSL(WebhooksAppTest):
 
         self.assertEquals(response_parsed['success'], True)
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(self.results[0]['test_run_webserver_with_ssl_post'][0], 'SOMETESTVALUE')
+        self.assertEquals(self.results[0]['test_run_webserver_with_post'][0], 'SOMETESTVALUE')
+
+    @classmethod
+    def setUpClass(cls):
+        cls.start_server_thread()
+
+class TestWebhooksServerSSL(TestWebhooksServer):
+    """
+    This tests the input ability to work with SSL.
+    """
+    protocol = 'https'
 
 if __name__ == "__main__":
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='../tmp/test_reports'))
+    test_dir = '../../tmp/test_reports'
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=test_dir))
